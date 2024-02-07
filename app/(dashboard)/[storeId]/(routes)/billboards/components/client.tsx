51e8b7283cc7b9eea03d35b94b3c0113da30c477
B-Phan <brandon.phan2@ucalgary.ca>
2024-02-06 20:59:42 -0700

    data table files added

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,15 +5,22 @@ import { Heading } from "@/components/ui/heading";
 import { Plus } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn } from "./columns";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+  data: BillboardColumn[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+  data
+}) => {
     const router = useRouter();
     const params = useParams();
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards (0)"
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button onClick = {() => router.push(`/${params.storeId}/billboards/new`)}>
