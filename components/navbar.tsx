e078dea385b4f063d7ad843985357dccfcaf36a5
B-Phan <brandon.phan2@ucalgary.ca>
2024-02-19 14:43:34 -0700

    added categories in navigation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,9 +4,17 @@ import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 import getCategories from "@/actions/get-categories";
 
+import getCategory from "@/actions/get-categories";
+
 const Navbar = async () => {
   const categories = await getCategories();
+
+  const cat = await getCategory();
+
   console.log(categories); 
+  console.log(cat); 
+
+
 
   return ( 
     <div className="border-b">
@@ -15,7 +23,7 @@ const Navbar = async () => {
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">STORE</p>
           </Link>
-          {/* <MainNav data={categories} /> */}
+          <MainNav data={categories} />
         </div>
       </Container>
     </div>
